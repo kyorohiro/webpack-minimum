de831bb8bd34617b0df38f8c184f85a1b4c7c04a
kiyohiro kawamura <kyorohiro@gmail.com>
2018-09-06 23:51:36 +0900

    ..

--- hello_use_lib/src/index.js
+++ hello_use_lib/src/index.js
@@ -2,14 +2,20 @@ import lodash from 'lodash';
 import './index.css';
 import icon from './1310669.jpeg';
 
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 function hello() {
 	console.log(lodash.join(['Hello', 'webpack'], ' '));
 }
 
 hello();
 
-
+/*
 function component() {
+	console.log("comp AA");
 	var element = document.createElement('div');
 
 	// Lodash, now imported by this script
@@ -23,3 +29,24 @@ function component() {
 }
 
 document.body.appendChild(component());
+*/
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+
+const app = new Vue({
+  router
+}).$mount('#app')
--- hello_use_lib/webpack.config.js
+++ hello_use_lib/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+      alias: {
+          'vue$': 'vue/dist/vue.esm.js'
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
   },
   mode: 'development',
   module: {
@@ -21,7 +32,18 @@ module.exports = {
         use: [
           "file-loader"
         ]
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
       }
   	]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+//    new VueLoaderPlugin()
+  ]
+
 };
